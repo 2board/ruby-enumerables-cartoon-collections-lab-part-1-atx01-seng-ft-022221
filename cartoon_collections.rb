1a11136190e2183380487ef88cbbd3d6c1a29f39
David Jun <djun301@gmail.com>
2021-02-14 01:54:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |x|
+  array.each_with_index do |x, index|
     puts x 
   end
   # Use `each_with_index` to enumerate over the provided array
